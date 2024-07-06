4a6d11cab0defbe34f42d03474dfccb8c1da18a0
Sulejmanov Artur <l0tus0rb@yandex.ru>
2024-07-06 12:51:57 +0200

    Скрипты установки БД

---
+++ install/db/install.sql
@@ -0,0 +1,35 @@
+CREATE TABLE b_hahariki_session
+(
+	ID int,
+	STATUS int,
+	OWNER_ID int,
+	STAGE int,
+	END_STAGE_TIME timestamp
+);
+
+CREATE TABLE b_hahariki_session_user
+(
+	USER_ID int,
+	SESSION_ID int
+);
+
+CREATE TABLE b_hahariki_session_status
+(
+	ID int,
+	TITLE varchar(20)
+);
+
+CREATE TABLE b_hahariki_anekdot
+(
+	ID int,
+	SESSION_ID int,
+	CATEGORY varchar(100)
+);
+
+CREATE TABLE b_hahariki_anekdot_part
+(
+	ID int,
+	TEXT varchar(200),
+	AUTHOR_ID int,
+	PARENT_ID int NULL
+)
---
+++ install/db/uninstall.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS b_hahariki_session;
+DROP TABLE IF EXISTS b_hahariki_session_user;
+DROP TABLE IF EXISTS b_hahariki_session_status;
+DROP TABLE IF EXISTS b_hahariki_anekdot;
+DROP TABLE IF EXISTS b_hahariki_anekdot_part;
