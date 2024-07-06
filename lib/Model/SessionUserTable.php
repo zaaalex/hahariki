5a3ad674d390563bd36728f2a3a801e320d4a865
Sulejmanov Artur <l0tus0rb@yandex.ru>
2024-07-06 16:48:44 +0200

    Фикс сохранения сесии и пользоваетль

--- lib/Model/SessionUserTable.php
+++ lib/Model/SessionUserTable.php
@@ -1,11 +1,9 @@
 <?php
 namespace Bitrix\Hahariki\Model;
-
-use Bitrix\Main\Entity\ReferenceField;
 use Bitrix\Main\Localization\Loc;
 use Bitrix\Main\ORM\Data\DataManager;
 use Bitrix\Main\ORM\Fields\IntegerField;
-use Bitrix\Main\ORM\Fields\Relations\OneToMany;
+use Bitrix\Main\ORM\Fields\Relations\Reference;
 use Bitrix\Main\ORM\Query\Join;
 use Bitrix\Main\UserTable;
 
@@ -46,21 +44,23 @@ class SessionUserTable extends DataManager
 			))->configureTitle(Loc::getMessage('SESSION_USER_ENTITY_USER_ID_FIELD'))
 				->configurePrimary(true)
 			,
-			(new ReferenceField(
+			(new Reference(
 				'USER',
 				UserTable::class,
 				Join::on('this.USER_ID', 'ref.ID')
 			))
+			,
 			(new IntegerField('SESSION_ID',
 							  []
 			))->configureTitle(Loc::getMessage('SESSION_USER_ENTITY_SESSION_ID_FIELD'))
 				->configurePrimary(true)
 			,
-			(new ReferenceField(
+			(new Reference(
 				'SESSION',
 				SessionTable::class,
 				Join::on('this.SESSION_ID', 'ref.ID')
 			))
+			,
 		];
 	}
 }
