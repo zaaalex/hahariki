aa5747d71ba34903bbac1648c1eda07296c9c542
Artem Kalinichenko <klnkklnk@icloud.com>
2024-07-06 14:06:22 +0200

    Controller Lobby and Filter CheckLobbyOwner

--- .settings.php
+++ .settings.php
@@ -1,1 +1,13 @@
 <?php
+
+return [
+	'controllers' => [
+		'value' => [
+			'namespaces' => [
+				//'\\Bitrix\\Hahariki\\Controller\\' => 'haha',
+			],
+			'defaultNamespace' => '\\Bitrix\\Hahariki\\Controller',
+		],
+		'readonly' => true,
+	],
+];
---
+++ lib/Controller/Lobby.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Bitrix\Hahariki\Controller;
+
+use Bitrix\Hahariki\Controller\BaseController;
+use Bitrix\Hahariki\Controller\Filter\CheckLobbyOwner;
+use Bitrix\Main\Engine\AutoWire\ExactParameter;
+use Bitrix\Main\Engine\CurrentUser;
+
+class Lobby extends BaseController
+{
+	public function configureActions()
+	{
+		return [
+			'startGame' => [
+				'+prefilters' => [
+					new CheckLobbyOwner(),
+				],
+			],
+		];
+	}
+
+	public function getPrimaryAutoWiredParameter()
+	{
+//		return new ExactParameter(
+//			\Bitrix\Im\V2\Chat::class,
+//			'chat',
+//			function ($className, $id) {
+//				return \Bitrix\Im\V2\Chat::getInstance((int)$id);
+//			}
+//		);
+	}
+
+	/**
+	 * @restMethod
+	 */
+	public function createLobbyAction(CurrentUser $lobbyOwner): array
+	{
+		return [
+			'test' => 'test',
+		];
+	}
+
+	/**
+	 * @restMethod
+	 */
+	public function startGameAction(CurrentUser $lobbyOwner, int $sessionId): array
+	{
+		return [
+			'owner' => $lobbyOwner,
+			'sessionID' => $sessionId,
+		];
+	}
+
+	/**
+	 * @restMethod
+	 */
+	public function saveCategoryAction(CurrentUser $currentUser): array
+	{
+		return [
+			'test' => 'test',
+		];
+	}
+
+	/**
+	 * @restMethod
+	 */
+	public function savePartAction(CurrentUser $currentUser): array
+	{
+		return [
+			'test' => 'test',
+		];
+	}
+
+	/**
+	 * @restMethod
+	 */
+	public function getDataAction(CurrentUser $currentUser): array
+	{
+		return [
+			'test' => 'test',
+		];
+	}
+}
